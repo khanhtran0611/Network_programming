96a5d1bfda69a3ff9be67e129c2951c6eb97bbb8
root <root@DESKTOP-0MKBRF3.localdomain>
2025-11-04 03:09:32 +0000

    add newest version

--- client.c
+++ client.c
@@ -12,10 +12,8 @@
 #include <stdint.h> // For uint32_t
 
 struct sockaddr_in server_addr; // client_addr and c are not used in client.c
+GtkListStore *file_list_store;
 
-// void openGroupFolder()[
-
-// ]
 
 // A helper function to create a new row for the members list
 GtkWidget *create_member_row(const gchar *email)
@@ -173,43 +171,89 @@ void on_upload_file_clicked(GtkButton *button, gpointer user_data)
 // Callback for the "Add Folder" button
 void on_add_folder_clicked(GtkButton *button, gpointer user_data)
 {
+    int signal = 0;
     GtkListStore *file_list_store = GTK_LIST_STORE(user_data);
     GtkWidget *dialog;
-    // Use GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER to select folders
-    GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER;
-    gint res;
+    GtkWidget *content_area;
+    GtkWidget *entry;
+    gchar *foldername = NULL;
 
-    // Get the main window to be the parent of the dialog
+    // Lấy cửa sổ chính để làm cha cho dialog
     GtkWidget *toplevel = gtk_widget_get_toplevel(GTK_WIDGET(button));
 
-    dialog = gtk_file_chooser_dialog_new("Select Folder",
+    // Tạo dialog mới với các nút "Create" và "Cancel"
+    dialog = gtk_dialog_new_with_buttons("Create New Folder",
                                          GTK_WINDOW(toplevel),
-                                         action,
-                                         "_Cancel",
-                                         GTK_RESPONSE_CANCEL,
-                                         "_Select",
+                                         GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+                                         "_Create",
                                          GTK_RESPONSE_ACCEPT,
+                                         "_Cancel",
+                                         GTK_RESPONSE_REJECT,
                                          NULL);
 
+    // Lấy vùng nội dung của dialog
+    content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+
+    // Tạo một GtkEntry để người dùng nhập tên thư mục
+    entry = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(entry), "Enter folder name...");
+    gtk_widget_set_margin_start(entry, 10);
+    gtk_widget_set_margin_end(entry, 10);
+    gtk_widget_set_margin_top(entry, 10);
+    gtk_widget_set_margin_bottom(entry, 10);
+    gtk_container_add(GTK_CONTAINER(content_area), entry);
+    gtk_widget_show(entry);
+
+    
+
+    gint res;
     res = gtk_dialog_run(GTK_DIALOG(dialog));
     if (res == GTK_RESPONSE_ACCEPT)
     {
-        char *folderpath;
-        GtkFileChooser *chooser = GTK_FILE_CHOOSER(dialog);
-        folderpath = gtk_file_chooser_get_filename(chooser);
+        const gchar *text = gtk_entry_get_text(GTK_ENTRY(entry));
+        if (text && *text) // Check if the text is not null and not empty
+        {
+            foldername = g_strdup(text); // Make a copy
+            g_print("Folder to create: %s\n", foldername);
 
-        gchar *basename = g_path_get_basename(folderpath);
-        g_print("Folder selected: %s\n", basename);
-
-        GtkTreeIter iter;
-        gtk_list_store_append(file_list_store, &iter);
-        gtk_list_store_set(file_list_store, &iter, 0, basename, 1, "Folder", -1);
-
-        g_free(basename);
-        g_free(folderpath);
+            // Thêm vào giao diện trước để có cảm giác phản hồi nhanh
+            GtkTreeIter iter;
+            gtk_list_store_append(file_list_store, &iter);
+            gtk_list_store_set(file_list_store, &iter, 0, foldername, 1, "Folder", -1);
+            signal = 1;
+        }
     }
-
+    char* my_folder_name = foldername;
     gtk_widget_destroy(dialog);
+
+    if (signal == 1)
+    {
+        int s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // Use local s
+        if (s == -1)
+        {
+            perror("socket");
+            exit(1);
+        }
+        if (connect(s, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+        {
+            perror("connect");
+            exit(1);
+        }
+        if (send(s, "addFolder", 1024, 0) < 0)
+        {
+            perror("send");
+            exit(1);
+        }
+
+        if (send(s, my_folder_name, 1024, 0) < 0)
+        {
+            perror("send");
+            exit(1);
+        }
+        close(s);
+        signal = 0;
+    }
+    g_free(foldername);
 }
 
 // --- START: Right-click menu functions ---
@@ -228,6 +272,108 @@ void on_menu_download_activate(GtkMenuItem *menuitem, gpointer user_data)
     g_free(filename); // Free the duplicated filename string
 }
 
+// Callback function when "Rename" is selected from the context menu
+void on_menu_rename_activate(GtkMenuItem *menuitem, gpointer user_data)
+{
+    GtkTreePath *path = (GtkTreePath *)user_data;
+    GtkWidget *menu = gtk_widget_get_parent(GTK_WIDGET(menuitem));
+    GtkTreeModel *model = (GtkTreeModel *)g_object_get_data(G_OBJECT(menu), "target-model");
+    GtkTreeIter iter;
+    gchar *old_filename = NULL;
+    gchar *new_filename;
+    if (model && path && gtk_tree_model_get_iter(model, &iter, path))
+    {
+        gtk_tree_model_get(model, &iter, 0, &old_filename, -1);
+
+        // Create a dialog to get the new name
+        GtkWidget *toplevel = gtk_widget_get_toplevel(GTK_WIDGET(menu));
+        GtkWidget *dialog = gtk_dialog_new_with_buttons("Rename Item",
+                                                        GTK_WINDOW(toplevel),
+                                                        GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                        "_Rename", GTK_RESPONSE_ACCEPT,
+                                                        "_Cancel", GTK_RESPONSE_REJECT,
+                                                        NULL);
+        GtkWidget *content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+        GtkWidget *entry = gtk_entry_new();
+        gtk_entry_set_text(GTK_ENTRY(entry), old_filename);
+        gtk_container_add(GTK_CONTAINER(content_area), entry);
+        gtk_widget_show(entry);
+ 
+
+        int signal = 0;
+
+        gint response = gtk_dialog_run(GTK_DIALOG(dialog));
+        if (response == GTK_RESPONSE_ACCEPT)
+        {
+            const gchar *new_filename_const = gtk_entry_get_text(GTK_ENTRY(entry));
+            if (new_filename_const && *new_filename_const && g_strcmp0(old_filename, new_filename_const) != 0)
+            {
+                new_filename = g_strdup(new_filename_const);
+                g_print("Renaming '%s' to '%s'\n", old_filename, new_filename);
+
+                // Send rename command to server
+
+            }
+            signal = 1;
+        }
+
+        gtk_widget_destroy(dialog);
+        if(signal == 1){
+        int s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (s == -1)
+        {
+            perror("socket");
+            exit(1);
+        }
+        if (connect(s, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+        {
+            perror("connect");
+            exit(1);
+        }
+            
+        if(send(s, "rename", 1024, 0) < 0) 
+        {
+            perror("send");
+            exit(1);
+        }
+        if(send(s, old_filename, 1024, 0) < 0) 
+        {
+            perror("send");
+            exit(1);
+        }
+        if(send(s, new_filename, 1024, 0) < 0) 
+        {
+            perror("send");
+            exit(1);
+        }
+        close(s);
+
+                    // Update the model
+        gtk_list_store_set(GTK_LIST_STORE(model), &iter, 0, new_filename, -1);
+                
+        }
+        g_free(new_filename);
+        g_free(old_filename);
+    }
+    gtk_tree_path_free(path);
+}
+
+// Callback function when "View" is selected from the context menu
+void on_menu_view_activate(GtkMenuItem *menuitem, gpointer user_data)
+{
+    char *foldername = (char *)user_data;
+    g_print("Context menu: 'View' clicked for folder: %s\n", foldername);
+        if (file_list_store) {
+        gtk_list_store_clear(file_list_store);
+    } else {
+        g_printerr("file_list_store is NULL. Cannot clear.\n");
+        g_free(foldername);
+        return;
+    }
+
+    
+    g_free(foldername);
+}
 // Callback function when "Delete" is selected from the context menu
 void on_menu_delete_activate(GtkMenuItem *menuitem, gpointer user_data)
 {
@@ -237,14 +383,17 @@ void on_menu_delete_activate(GtkMenuItem *menuitem, gpointer user_data)
     GtkWidget *menu = gtk_widget_get_parent(GTK_WIDGET(menuitem));
     GtkTreeModel *model = (GtkTreeModel *)g_object_get_data(G_OBJECT(menu), "target-model");
     gchar *filename;
+    gchar *type;
     char my_basename[1024];
+    char *command_to_send = NULL;
     if (model && path)
     {
         GtkTreeIter iter;
         if (gtk_tree_model_get_iter(model, &iter, path))
         {
-            gtk_tree_model_get(model, &iter, 0, &filename, -1);
-            g_print("Context menu: 'Delete' clicked for item: %s\n", filename);
+            // Lấy cả tên (cột 0) và loại (cột 1)
+            gtk_tree_model_get(model, &iter, 0, &filename, 1, &type, -1);
+            g_print("Context menu: 'Delete' clicked for item: %s, type: %s\n", filename, type);
 
             // Remove the row from the list store
             gtk_list_store_remove(GTK_LIST_STORE(model), &iter);
@@ -252,10 +401,18 @@ void on_menu_delete_activate(GtkMenuItem *menuitem, gpointer user_data)
 
             signal = 1;
             // g_free(filename);
+
+            // Xác định lệnh cần gửi dựa trên loại
+            if (g_strcmp0(type, "folder") == 0) {
+                command_to_send = "deleteFolder";
+            } else {
+                command_to_send = "deleteFile";
+            }
+            g_free(type); // Giải phóng bộ nhớ cho 'type'
         }
     }
     snprintf(my_basename, sizeof(my_basename), "%s", filename);
-    if (signal == 1)
+    if (signal == 1 && command_to_send != NULL)
     {
         int s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // Use local s
         if (s == -1)
@@ -270,7 +427,7 @@ void on_menu_delete_activate(GtkMenuItem *menuitem, gpointer user_data)
             perror("connect");
             exit(1);
         }
-        if (send(s, "deleteFile", 1024, 0) < 0)
+        if (send(s, command_to_send, 1024, 0) < 0)
         {
             printf("3\n");
             perror("send");
@@ -303,21 +460,34 @@ gboolean on_files_treeview_button_press(GtkWidget *treeview, GdkEventButton *eve
             // Create the context menu
             GtkWidget *menu = gtk_menu_new();
 
+            GtkWidget *item_view = gtk_menu_item_new_with_label("View");
             GtkWidget *item_download = gtk_menu_item_new_with_label("Download");
+            GtkWidget *item_rename = gtk_menu_item_new_with_label("Rename");
             GtkWidget *item_delete = gtk_menu_item_new_with_label("Delete");
-
+            
+            gtk_menu_shell_append(GTK_MENU_SHELL(menu), item_view);
             gtk_menu_shell_append(GTK_MENU_SHELL(menu), item_download);
+            gtk_menu_shell_append(GTK_MENU_SHELL(menu), item_rename);
             gtk_menu_shell_append(GTK_MENU_SHELL(menu), item_delete);
+            
 
-            // Get filename to pass to download callback
+            // Get filename and type to decide which items to enable
             GtkTreeModel *model = gtk_tree_view_get_model(GTK_TREE_VIEW(treeview));
             GtkTreeIter iter;
             gtk_tree_model_get_iter(model, &iter, path);
             gchar *filename;
-            gtk_tree_model_get(model, &iter, 0, &filename, -1);
+            gchar *type;
+            gtk_tree_model_get(model, &iter, 0, &filename, 1, &type, -1);
+
+            // "View" is only for folders
+            if (g_strcmp0(type, "folder") != 0) {
+                gtk_widget_set_sensitive(item_view, FALSE);
+            }
 
             // Connect signals for menu items
+            g_signal_connect(item_view, "activate", G_CALLBACK(on_menu_view_activate), g_strdup(filename));
             g_signal_connect(item_download, "activate", G_CALLBACK(on_menu_download_activate), g_strdup(filename));
+            g_signal_connect(item_rename, "activate", G_CALLBACK(on_menu_rename_activate), gtk_tree_path_copy(path));
             g_signal_connect(item_delete, "activate", G_CALLBACK(on_menu_delete_activate), gtk_tree_path_copy(path));
             g_object_set_data(G_OBJECT(menu), "target-model", model); // Store model for delete callback
 
@@ -325,6 +495,7 @@ gboolean on_files_treeview_button_press(GtkWidget *treeview, GdkEventButton *eve
             gtk_menu_popup_at_pointer(GTK_MENU(menu), (GdkEvent *)event);
 
             g_free(filename);
+            g_free(type);
             gtk_tree_path_free(path);
 
             return TRUE; // Event handled, stop propagation
@@ -501,11 +672,15 @@ void get_list_of_files(GtkListStore *file_list_store) // Removed iter parameter
     close(s); // Close the local socket
 }
 
+void viewOpeningFolder(GtkListStore *file_list_store){
+
+}
+
 int main(int argc, char *argv[])
 {
     GtkBuilder *builder;
     GtkWidget *window, *group_name_label, *members_listbox, *upload_button, *add_folder_button, *files_treeview;
-    GtkListStore *file_list_store; // Thay đổi từ GtkTreeStore sang GtkListStore
+ // Thay đổi từ GtkTreeStore sang GtkListStore
     // GtkTreeIter iter; // Removed iter declaration
     GError *error = NULL;
 
@@ -569,26 +744,6 @@ int main(int argc, char *argv[])
     // 3.4. Xóa dữ liệu mẫu và thêm file/folder mới vào GtkListStore
     gtk_list_store_clear(file_list_store);
 
-    // --- Thêm dữ liệu dạng danh sách phẳng ---
-
-    // Thêm folder "Source Code"
-    // gtk_list_store_append(file_list_store, &iter);
-    // gtk_list_store_set(file_list_store, &iter, 0, "Source Code", 1, "Folder", -1);
-
-    // // Thêm file "ui_test.c"
-    // gtk_list_store_append(file_list_store, &iter);
-    // gtk_list_store_set(file_list_store, &iter, 0, "ui_test.c", 1, "File", -1);
-
-    // // Thêm folder "Documents"
-    // gtk_list_store_append(file_list_store, &iter);
-    // gtk_list_store_set(file_list_store, &iter, 0, "Documents", 1, "Folder", -1);
-
-    // // Thêm file "report.docx"
-    // gtk_list_store_append(file_list_store, &iter);
-    // gtk_list_store_set(file_list_store, &iter, 0, "report.docx", 1, "File", -1);
-
-    // --- Kết thúc thêm dữ liệu ---
-
     // Kết nối sự kiện cho nút Upload File
     g_signal_connect(upload_button, "clicked", G_CALLBACK(on_upload_file_clicked), file_list_store);
     g_signal_connect(add_folder_button, "clicked", G_CALLBACK(on_add_folder_clicked), file_list_store);
@@ -611,127 +766,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-// {
-//     perror("recv");
-//     exit(1);
-// }
-// filename[bytes_recv] = '\0';
-// printf("%s\n", filename);
-// // if (strcmp(filename, "FIN!") == 0)
-// // {
-// //     break;
-// // }
-// bytes_recv = recv(s, type, 10, 0);
-// type[bytes_recv] = '\0';
-// printf("%s\n", type);
-// insert_list_tree(filename, type, file_list_store, iter);
-// }
-// close(s);
-// }
-
-// int main(int argc, char *argv[])
-// {
-//     GtkBuilder *builder;
-//     GtkWidget *window, *group_name_label, *members_listbox, *upload_button, *add_folder_button;
-//     GtkListStore *file_list_store; // Thay đổi từ GtkTreeStore sang GtkListStore
-//     GtkTreeIter iter;
-//     GError *error = NULL;
-
-//     // 1. Khởi tạo môi trường GTK+
-//     gtk_init(&argc, &argv);
-
-//     // 2. Tạo GtkBuilder và tải tệp .glade
-//     builder = gtk_builder_new();
-//     if (gtk_builder_add_from_file(builder, "group_members_screen.glade", &error) == 0)
-//     {
-//         // Xử lý lỗi nếu không tải được tệp
-//         g_printerr("Lỗi khi tải tệp UI: %s\n", error->message);
-//         g_clear_error(&error);
-//         return 1;
-//     }
-
-//     // 3. Lấy widget cửa sổ chính (sử dụng ID bạn đặt trong Glade)
-//     window = GTK_WIDGET(gtk_builder_get_object(builder, "group_members_window"));
-
-//     // --- Bắt đầu thêm dữ liệu vào UI ---
-
-//     // 3.1. Lấy con trỏ tới các widget khác bằng ID
-//     group_name_label = GTK_WIDGET(gtk_builder_get_object(builder, "group_name_label"));
-//     members_listbox = GTK_WIDGET(gtk_builder_get_object(builder, "members_listbox"));
-//     file_list_store = GTK_LIST_STORE(gtk_builder_get_object(builder, "file_list_store")); // Ép kiểu sang GtkListStore
-//     upload_button = GTK_WIDGET(gtk_builder_get_object(builder, "upload_file_button"));    // Giả sử ID của nút là "upload_file_button"
-//     add_folder_button = GTK_WIDGET(gtk_builder_get_object(builder, "add_folder_button"));
-
-//     // 3.2. Thay đổi tên nhóm
-//     gtk_label_set_text(GTK_LABEL(group_name_label), "Members of 'C Project Group'");
-
-//     // 3.3. Xóa dữ liệu mẫu và thêm thành viên mới vào GtkListBox
-//     // Lấy tất cả các con của listbox và xóa chúng
-//     GList *children, *iter_list;
-//     children = gtk_container_get_children(GTK_CONTAINER(members_listbox));
-//     for (iter_list = children; iter_list != NULL; iter_list = g_list_next(iter_list))
-//     {
-//         gtk_widget_destroy(GTK_WIDGET(iter_list->data));
-//     }
-//     g_list_free(children);
-
-//     // Thêm các thành viên mới
-//     const gchar *members[] = {"alice@example.com", "bob@example.com", "charlie@example.com", NULL};
-//     for (int i = 0; members[i] != NULL; i++)
-//     {
-//         GtkWidget *new_row = create_member_row(members[i]);
-//         gtk_container_add(GTK_CONTAINER(members_listbox), new_row);
-
-//         // SỬA LỖI: Lấy button từ GtkBox một cách chính xác
-//         // GtkListBoxRow là một GtkBin, con của nó là GtkBox.
-//         GtkWidget *box_in_row = gtk_bin_get_child(GTK_BIN(new_row));
-//         // GtkBox chứa nhiều con. Lấy danh sách các con và lấy widget cuối cùng (là button).
-//         GList *box_children = gtk_container_get_children(GTK_CONTAINER(box_in_row));
-//         GtkWidget *button = g_list_last(box_children)->data;
-//         g_list_free(box_children); // Giải phóng danh sách sau khi dùng
-
-//         g_signal_connect(button, "clicked", G_CALLBACK(on_remove_clicked), NULL);
-//     }
-
-//     // 3.4. Xóa dữ liệu mẫu và thêm file/folder mới vào GtkListStore
-//     gtk_list_store_clear(file_list_store);
-
-//     // --- Thêm dữ liệu dạng danh sách phẳng ---
-
-//     // Thêm folder "Source Code"
-//     // gtk_list_store_append(file_list_store, &iter);
-//     // gtk_list_store_set(file_list_store, &iter, 0, "Source Code", 1, "Folder", -1);
-
-//     // // Thêm file "ui_test.c"
-//     // gtk_list_store_append(file_list_store, &iter);
-//     // gtk_list_store_set(file_list_store, &iter, 0, "ui_test.c", 1, "File", -1);
-
-//     // // Thêm folder "Documents"
-//     // gtk_list_store_append(file_list_store, &iter);
-//     // gtk_list_store_set(file_list_store, &iter, 0, "Documents", 1, "Folder", -1);
-
-//     // // Thêm file "report.docx"
-//     // gtk_list_store_append(file_list_store, &iter);
-//     // gtk_list_store_set(file_list_store, &iter, 0, "report.docx", 1, "File", -1);
-
-//     // --- Kết thúc thêm dữ liệu ---
-
-//     // Kết nối sự kiện cho nút Upload File
-//     g_signal_connect(upload_button, "clicked", G_CALLBACK(on_upload_file_clicked), file_list_store);
-//     g_signal_connect(add_folder_button, "clicked", G_CALLBACK(on_add_folder_clicked), file_list_store);
-
-//     // Đặt hàm xử lý sự kiện đóng cửa sổ
-//     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-
-//     server_addr.sin_family = AF_INET;
-//     server_addr.sin_port = htons(8080);
-//     server_addr.sin_addr.s_addr = inet_addr("172.29.207.94");
-
-//     get_list_of_files(file_list_store, iter);
-
-//     // 4. Hiển thị cửa sổ và bắt đầu vòng lặp chính của ứng dụng
-//     gtk_widget_show_all(window);
-//     gtk_main();
-
-//     return 0;
-// }
--- server.c
+++ server.c
@@ -8,6 +8,7 @@
 #include <netinet/ip.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/stat.h>
 #include <string.h>
 #include <dirent.h>
 
@@ -120,6 +121,88 @@ void get_list_of_files(int c)
     }
 }
 
+void viewFolder(int c){
+    char foldername[1024];
+    int bytes_recv = recv(c, foldername, 1024, 0);
+    if (bytes_recv < 0)
+    {
+        perror("recv");
+        exit(1);
+    }
+    foldername[bytes_recv] = '\0';
+    char full_path[10240];
+    strcpy(full_path, BASE_PATH);
+    strcat(full_path, foldername);
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(full_path);
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            if (strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0)
+            {
+                // Send filename length
+                uint32_t name_len = strlen(dir->d_name) + 1; // Include null terminator
+                uint32_t net_name_len = htonl(name_len);
+                if (send(c, &net_name_len, sizeof(net_name_len), 0) < 0)
+                {
+                    perror("send name_len");
+                    exit(1);
+                }
+                // Send filename
+                if (send(c, dir->d_name, name_len, 0) < 0)
+                {
+                    perror("send filename");
+                    exit(1);
+                }
+                printf("%s\n", dir->d_name);
+                char *type;
+                if (dir->d_type == DT_DIR)
+                {
+                    type = "folder";
+                }
+                else
+                {
+                    type = "file";
+                }
+                printf("%s\n", type);
+                // Send type length
+                uint32_t type_len = strlen(type) + 1; // Include null terminator
+                uint32_t net_type_len = htonl(type_len);
+                if (send(c, &net_type_len, sizeof(net_type_len), 0) < 0)
+                {
+                    perror("send type_len");
+                    exit(1);
+                }
+                // Send type
+                if (send(c, type, type_len, 0) < 0)
+                {
+                    perror("send type");
+                    exit(1);
+                }
+            }
+        }
+        closedir(d);
+        // Signal end of list by sending a zero length for the next item
+        uint32_t zero_len = htonl(0);
+        if (send(c, &zero_len, sizeof(zero_len), 0) < 0)
+        {
+            perror("send zero_len");
+            exit(1);
+        }
+        if (shutdown(c, SHUT_WR) < 0)
+        {
+            perror("shutdown");
+        }
+    }
+    else
+    {
+        perror("opendir");
+        exit(1);
+    }
+}
+
 void deleteFile(int c)
 {
     char filename[1024];
@@ -141,6 +224,84 @@ void deleteFile(int c)
         exit(1);
     }
     printf("Sucessfully deleted file : %s\n", filename);
+}
+
+void addFolder(int c){
+    char foldername[1024];
+    int bytes_recv = recv(c, foldername, 1024, 0);
+    if (bytes_recv < 0)
+    {
+        perror("recv");
+        return; // Don't exit the whole server
+    }
+    foldername[bytes_recv] = '\0';
+    char full_path[512];
+    strcpy(full_path, BASE_PATH);
+    strcat(full_path, foldername);
+    if (mkdir(full_path, 0755) == 0) { // Use 0755 for standard permissions
+        printf("Successfully created folder: %s\n", foldername);
+    } else {
+        perror("mkdir"); 
+    }
+}
+
+void deleteFolder(int c){
+    char foldername[1024];
+    int bytes_recv = recv(c, foldername, 1024, 0);
+    if (bytes_recv < 0)
+    {
+        perror("recv");
+        return; // Don't exit the whole server
+    }
+    foldername[bytes_recv] = '\0';
+    char full_path[512];
+    strcpy(full_path, BASE_PATH);
+    strcat(full_path, foldername);
+    char command[1024];
+    strcpy(command, "rm -rf ");
+    strcat(command, full_path);
+    if(system(command) == 0){
+        printf("Successfully deleted folder: %s\n", foldername);
+    }else{
+        perror("system");
+    }
+}
+
+void renameFileOrFolder(int c) {
+    char old_name[1024];
+    char new_name[1024];
+    int bytes_recv;
+
+    // Receive old name
+    bytes_recv = recv(c, old_name, 1024, 0);
+    if (bytes_recv <= 0) {
+        perror("recv old_name");
+        return;
+    }
+    old_name[bytes_recv] = '\0';
+    printf("Received old name: %s\n", old_name);
+
+    // Receive new name
+    bytes_recv = recv(c, new_name, 1024, 0);
+    if (bytes_recv <= 0) {
+        perror("recv new_name");
+        return;
+    }
+    printf("Received new name: %s\n", new_name);
+    new_name[bytes_recv] = '\0';
+
+    char old_full_path[2048];
+    char new_full_path[2048];
+
+    snprintf(old_full_path, sizeof(old_full_path), "%s%s", BASE_PATH, old_name);
+    snprintf(new_full_path, sizeof(new_full_path), "%s%s", BASE_PATH, new_name);
+
+    if (rename(old_full_path, new_full_path) == 0) {
+        printf("Successfully renamed '%s' to '%s'\n", old_name, new_name);
+    } else {
+        perror("rename");
+        // Optionally, send an error message back to the client
+    }
 }
 
 void handle_client(int c)
@@ -167,6 +328,18 @@ void handle_client(int c)
     else if (strcmp(command, "deleteFile") == 0)
     {
         deleteFile(c);
+    }else if (strcmp(command, "addFolder") == 0)
+    {
+        addFolder(c);
+    }else if (strcmp(command, "deleteFolder") == 0)
+    {
+        deleteFolder(c);
+    }else if (strcmp(command, "viewFolder") == 0)
+    {  
+       viewFolder(c);
+    }else if (strcmp(command, "rename") == 0)
+    {
+       renameFileOrFolder(c);
     }
     else
     {
